cd2fa403c127fe2f181e8f11614dae41847f8949
chao <chen404i@outlook.com>
2017-04-21 12:01:54 +0800

    Sync constant
    同步 APIHelper

--- src/com/chao117/base/constant/APIHelper.java
+++ src/com/chao117/base/constant/APIHelper.java
@@ -1,36 +1,64 @@
 package com.chao117.base.constant;
 
 public interface APIHelper {
-	//服务器通信用标记
+    //服务器通信用标记
     String JSON = "json";
-    String REQUEST_CODE = "requestCode";
-
-    //返回 json 串必须有的类型
-    String KIND = "kind";
-    String ERROR_CODE = "errorCode";
-    String RESULT = "result";
-
-
-    String FIELD_WHAT = "what";
-
-    String OBJECT = "object";
-    String OBJECT_ARRY = "objectArray";
-
-    //kind
-    String KIND_OBJ = "kindObj";
-    String KIND_ARR = "kindArr";
 
     //result
     String RESULT_SUCCESS = "resultSuccess";
     String RESULT_FAILURE = "resultFailure";
 
- 
-    //请求的 api 功能
+    /**
+     * api 1.0
+     */
     String API_TYPE_TEST = "test";
-    String API_TYPE_LOGIN_BAK ="loginbak";
+    String API_TYPE_LOGIN_BAK = "loginbak";
+    String API_TYPE_CHECK_USER = "forUser";
+
+    /**
+     * api 2.0
+     */
     //api 2.0:使用 ServerResult 返回结果
     String API_TYPE_LOGIN = "login";
     String API_TYPE_REGISTER = "register";
     String API_TYPE_PUBLISH = "publish";
-    String API_TYPE_CHECK_USER = "forUser";
+    //2017年04月21日10:55:46
+    String API_TYPE_BROWSE = "simpleBrowse";                //简单浏览
+    String API_TYPE_PASSWORD = "password";                  //修改密码
+    String API_TYPE_FAV = "fav";                            //收藏商品
+    String API_TYPE_REQ_FAV = "requestFav";                 //请求收藏的商品
+    String API_TYPE_REQUEST_TRANSACTION = "transRequest";   //请求交易
+    String API_TYPE_ACCEPT_TRANSACTION = "transAccept";     //接受交易
+    String API_TYPE_CANCEL_TRANSACTION = "transCancel";     //取消交易
+    String API_TYPE_CHECK_TRANSACTION = "transCheck";       //查看交易状态
+    String API_TYPE_UPDATE_TRANSACTION = "transUpdate";     //更新交易状态
+
+    String API_TYPE_ADD_SHIP_ADDRESS = "addShipAddress";    //新增收货地址
+    String API_TYPE_REQ_SHIP_ADDRESS = "requestShipAddress";//请求收货地址
+    String API_TYPE_ADD_HISTORY = "addHistory";             //写入浏览历史
+    String API_TYPE_REQ_HISTORY = "requestHistory";         //请求浏览历史
+
+    String API_TYPE_CHECK_DB_VERSION = "checkDatabaseVer";  //检查本地服务器数据库版本号,先不实现
+    String API_TYPE_UPDATE_LOCAL_DB = "updateLocalDatabase";//更新本地数据库
+
+    String API_TYPE_SEARCH_GOODS = "searchGoods";           //搜索商品
+    String API_TYPE_REQUEST_MSGS = "requestMessages";       //请求 message 信息
+    String API_TYPE_PUBLISH_MSG = "publishMessage";         //发布留言
+
+
+    /**
+     * 以下为已经失效的常量
+     * api 1.0 阶段的常量
+     */
+
+    String REQUEST_CODE = "requestCode";
+    //返回 json 串必须有的类型
+    String KIND = "kind";
+    String ERROR_CODE = "errorCode";
+    String RESULT = "result";
+    String FIELD_WHAT = "what";
+    String OBJECT = "object";
+    String OBJECT_ARRY = "objectArray";
+    String KIND_OBJ = "kindObj";
+    String KIND_ARR = "kindArr";
 }
